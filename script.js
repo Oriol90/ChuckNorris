630e4c3e375d7c7139591c30d43702321c06bbf7
Oriol90 <58936038+Oriol90@users.noreply.github.com>
2020-02-25 12:16:10 +0100

    Elimino console log

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
         e.preventDefault();
         $.getJSON("http://api.icndb.com/jokes/random", 
             function (data, textStatus, jqXHR) {
-                console.log(data.value.joke);
                 $('#acudit').html(data.value.joke);
             });
     });
